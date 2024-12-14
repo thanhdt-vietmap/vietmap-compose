dfec623b0dea8bc4644d14e6872193b62ffd51fd
Sargun Vohra <sargun.vohra@gmail.com>
2024-12-14 00:51:55 -0800

    refactor: move feature expression functions into an object under the expression scope (#118)

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/expression/ExpressionScope.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/expression/ExpressionScope.kt
@@ -9,7 +9,6 @@ import kotlin.enums.enumEntries
 import kotlin.jvm.JvmName
 import kotlin.time.Duration
 
-// TODO make this use context parameters when available
 public object ExpressionScope {
   // region Literals
 
@@ -1099,53 +1098,57 @@ public object ExpressionScope {
 
   // region Feature data
 
-  /**
-   * Gets the feature properties object. Note that in some cases, it may be more efficient to use
-   * `get("property_name")` directly.
-   */
-  public fun properties(): Expression<MapValue> = callFn("properties").cast()
+  public object FeatureScope {
+    /**
+     * Gets the feature properties object. Note that in some cases, it may be more efficient to use
+     * `get("property_name")` directly.
+     */
+    public fun properties(): Expression<MapValue> = callFn("properties").cast()
 
-  /**
-   * **Note: Not supported on native platforms. See
-   * [maplibre-native#1698](https://github.com/maplibre/maplibre-native/issues/1698)**
-   *
-   * Retrieves a property value from the current feature's state. Returns `null` if the requested
-   * property is not present on the feature's state.
-   *
-   * A feature's state is not part of the GeoJSON or vector tile data, and must be set
-   * programmatically on each feature.
-   *
-   * When `source.promoteId` is not provided, features are identified by their `id` attribute, which
-   * must be an integer or a string that can be cast to an integer. When `source.promoteId` is
-   * provided, features are identified by their `promoteId` property, which may be a number, string,
-   * or any primitive data type. Note that [featureState] can only be used with layer properties
-   * that support data-driven styling.
-   */
-  // TODO: latest when featureState is supported on native platforms, should document which layer
-  //   properties support data-driven styling, i.e. featureState expressions.
-  public fun <T : ExpressionValue> featureState(key: Expression<StringValue>): Expression<T> =
-    callFn("feature-state", key).cast()
+    /**
+     * **Note: Not supported on native platforms. See
+     * [maplibre-native#1698](https://github.com/maplibre/maplibre-native/issues/1698)**
+     *
+     * Retrieves a property value from the current feature's state. Returns `null` if the requested
+     * property is not present on the feature's state.
+     *
+     * A feature's state is not part of the GeoJSON or vector tile data, and must be set
+     * programmatically on each feature.
+     *
+     * When `source.promoteId` is not provided, features are identified by their `id` attribute,
+     * which must be an integer or a string that can be cast to an integer. When `source.promoteId`
+     * is provided, features are identified by their `promoteId` property, which may be a number,
+     * string, or any primitive data type. Note that [state] can only be used with layer properties
+     * that support data-driven styling.
+     */
+    // TODO: latest when featureState is supported on native platforms, should document which layer
+    //   properties support data-driven styling, i.e. featureState expressions.
+    public fun <T : ExpressionValue> state(key: Expression<StringValue>): Expression<T> =
+      callFn("feature-state", key).cast()
 
-  /** Gets the feature's geometry type. */
-  public fun geometryType(): Expression<GeometryType> = callFn("geometry-type").cast()
+    /** Gets the feature's geometry type. */
+    public fun type(): Expression<GeometryType> = callFn("geometry-type").cast()
 
-  /** Gets the feature's id, if it has one. */
-  public fun <T : ExpressionValue> id(): Expression<T> = callFn("id").cast()
+    /** Gets the feature's id, if it has one. */
+    public fun <T : ExpressionValue> id(): Expression<T> = callFn("id").cast()
 
-  /**
-   * Gets the progress along a gradient line. Can only be used in the `gradient` property of a line
-   * layer, see [LineLayer][dev.sargunv.maplibrecompose.compose.layer.LineLayer].
-   */
-  public fun lineProgress(value: Expression<FloatValue>): Expression<FloatValue> =
-    callFn("line-progress", value).cast()
+    /**
+     * Gets the progress along a gradient line. Can only be used in the `gradient` property of a
+     * line layer, see [LineLayer][dev.sargunv.maplibrecompose.compose.layer.LineLayer].
+     */
+    public fun lineProgress(value: Expression<FloatValue>): Expression<FloatValue> =
+      callFn("line-progress", value).cast()
 
-  /**
-   * Gets the value of a cluster property accumulated so far. Can only be used in the
-   * `clusterProperties` option of a clustered GeoJSON source, see
-   * [GeoJsonOptions][dev.sargunv.maplibrecompose.core.source.GeoJsonOptions].
-   */
-  public fun <T : ExpressionValue> accumulated(key: Expression<StringValue>): Expression<T> =
-    callFn("accumulated", key).cast()
+    /**
+     * Gets the value of a cluster property accumulated so far. Can only be used in the
+     * `clusterProperties` option of a clustered GeoJSON source, see
+     * [GeoJsonOptions][dev.sargunv.maplibrecompose.core.source.GeoJsonOptions].
+     */
+    public fun <T : ExpressionValue> accumulated(key: Expression<StringValue>): Expression<T> =
+      callFn("accumulated", key).cast()
+  }
+
+  public val feature: FeatureScope = FeatureScope
 
   // endregion
 
