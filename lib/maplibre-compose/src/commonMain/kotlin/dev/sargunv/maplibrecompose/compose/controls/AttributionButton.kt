f9027ac967d05c95c9cb1a6d82b7f0fff2a837bd
Sargun Vohra <sargun.vohra@gmail.com>
2024-12-20 10:35:56 -0600

    refactor: simplify attribution button (#152)

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/compose/controls/AttributionButton.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/compose/controls/AttributionButton.kt
@@ -1,17 +1,23 @@
 package dev.sargunv.maplibrecompose.compose.controls
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import dev.sargunv.maplibrecompose.compose.StyleState
 
 @Composable
@@ -20,6 +26,7 @@ public fun AttributionButton(
   modifier: Modifier = Modifier,
   colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
   contentDescription: String = "Attribution",
+  confirmText: String = "OK",
 ) {
   var showDialog by remember { mutableStateOf(false) }
 
@@ -28,6 +35,24 @@ public fun AttributionButton(
   }
 
   if (showDialog) {
-    AttributionDialog(styleState, onDismiss = { showDialog = false })
+    val attributions = remember(styleState) { styleState.queryAttributionLinks() }
+    val uriHandler = LocalUriHandler.current
+
+    AlertDialog(
+      onDismissRequest = { showDialog = false },
+      confirmButton = {
+        TextButton(onClick = { showDialog = false }) {
+          Text(text = confirmText, style = MaterialTheme.typography.labelLarge)
+        }
+      },
+      title = { Text(text = "MapLibre Compose", style = MaterialTheme.typography.headlineSmall) },
+      text = {
+        Column {
+          attributions.forEach {
+            TextButton(onClick = { uriHandler.openUri(it.url) }) { Text(text = it.title) }
+          }
+        }
+      },
+    )
   }
 }
